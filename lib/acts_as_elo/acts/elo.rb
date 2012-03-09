1d23cf9a89d2909d8c9968dff64d3d823e619262
Nickolay Schwarz <tjbladez@gmail.com>
2012-03-09 16:14:17 -0600

    Support for active record, some documentation

--- lib/acts_as_elo/acts/elo.rb
+++ lib/acts_as_elo/acts/elo.rb
@@ -3,10 +3,15 @@ module Acts
     def self.included(base)
       base.extend(ClassMethods)
     end
-    
+
     module ClassMethods
+      # `acts_as_elo` hooks into your object to provide you with the ability to
+      # set and get `elo_rank` attribute
+      # Available options are:
+      # * default_rank - change the starting rank
+      # * one_way - limits update of the rank to only self
       def acts_as_elo(opts = {})
-        default_rank =  opts[:default_rank] || 1200 
+        default_rank =  opts[:default_rank] || 1200
 
         unless opts.empty?
           class << self
@@ -15,42 +20,53 @@ module Acts
           @acts_as_elo_options = opts
         end
 
-        class_eval do  
-          attr_writer :elo_rank
-          
-          define_method(:elo_rank) do
-            @elo_rank ||= default_rank
+        class_eval do
+          if Object::const_defined?("ActiveRecord") && self.ancestors.include?(ActiveRecord::Base)
+
+            define_method(:elo_rank) do
+              self[:elo_rank] ||= default_rank
+            end
+
+          else
+
+            attr_writer :elo_rank
+            define_method(:elo_rank) do
+              @elo_rank ||= default_rank
+            end
+
           end
         end
 
-        include Acts::Elo::InstanceMethods unless self.included_modules.include?(Acts::Elo::InstanceMethods)        
+        include Acts::Elo::InstanceMethods unless self.included_modules.include?(Acts::Elo::InstanceMethods)
       end
-    end   
-    
+    end
+
     module InstanceMethods
       def elo_win!(opponent, opts={})
         elo_update(opponent, opts.merge!(:result => :win))
       end
-      
+
       def elo_lose!(opponent, opts={})
         elo_update(opponent, opts.merge!(:result => :lose))
       end
-      
+
       def elo_draw!(opponent, opts={})
         elo_update(opponent, opts.merge!(:result => :draw))
       end
-      
+
       def elo_update(opponent, opts={})
         begin
           if self.class.respond_to?(:acts_as_elo_options) && self.class.acts_as_elo_options[:one_way]
             one_way = true
           end
           one_way   ||= opts[:one_way]
-          diff      = (opponent.elo_rank.to_f - elo_rank.to_f).abs
-          expected  = 1 / (1 + 10 ** (diff / 400))
           send_opts = {one_way: true}
 
-          if opts[:result] == :win 
+          # Formula from: http://en.wikipedia.org/wiki/Elo_rating_system
+          diff      = (opponent.elo_rank.to_f - elo_rank.to_f).abs
+          expected  = 1 / (1 + 10 ** (diff / 400))
+
+          if opts[:result] == :win
             points = 1
             send_opts.merge!(result: :lose)
           elsif opts[:result] == :lose
@@ -60,14 +76,14 @@ module Acts
             points = 0.5
             send_opts.merge!(result: :draw)
           end
-                    
+
           opponent.elo_update(self, send_opts) unless one_way
 
-          @elo_rank = (elo_rank + 10*(points-expected)).round          
+          self.elo_rank = (elo_rank + 10*(points-expected)).round
         rescue Exception => e
           puts "Exception: #{e.message}"
         end
       end
-    end 
+    end
   end
 end
--- test/advance_test.rb
+++ test/advance_test.rb
@@ -1,19 +1,18 @@
 require 'pathname'
 require Pathname.new('.') + 'helper'
 
-# When user answers the question based on the on current rank of the user 
+# When user answers the question based on the on current rank of the user
 # and current rank of the question both user and question ranks will be updated
 class User
   include Acts::Elo
-  
+
   attr_accessor :questions
-  
-  acts_as_elo :one_way => true
+  acts_as_elo
 end
 
 class Question
   include Acts::Elo
-  
+
   attr_accessor :user
   acts_as_elo
 end
@@ -21,25 +20,25 @@ end
 
 context "User with many questions" do
   helper(:question) { |user| Question.new.tap {|q| q.user = user} }
-  
+
   setup  { User.new }
   hookup { topic.questions = [question(topic), question(topic)] }
-  
+
   asserts(:elo_rank).equals(1200)
   asserts("questions have default ranks"){ topic.questions.map(&:elo_rank)}.equals([1200, 1200])
-  
+
   context "when question is answered correctly" do
     hookup { topic.questions.each {|q| q.elo_rank = 1200 }}
     hookup { topic.questions.first.elo_lose!(topic)}
-    
+
     asserts(:elo_rank).equals(1205)
     asserts("questions have their ranks updated"){ topic.questions.map(&:elo_rank)}.equals([1195, 1200])
   end
-  
+
   context "when question is answered incorrectly" do
     hookup { topic.questions.each {|q| q.elo_rank = 1200 }}
     hookup { topic.questions.first.elo_win!(topic) }
-        
+
     asserts(:elo_rank).equals(1195)
     asserts("questions have their ranks updated"){ topic.questions.map(&:elo_rank)}.equals([1205, 1200])
   end
